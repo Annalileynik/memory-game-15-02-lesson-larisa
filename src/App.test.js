e56f72909b007c82c0910fb2294a0977fce40e96
annadavydova <lileynik@yahoo.com>
2023-02-27 21:54:59 -0500

    initial3

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,8 @@
 import { render, screen } from ''@testing-library/react'';
-import App from ''./App'';
+import App from ''
+import react from "react";
+
+./App'';
 
 test(''renders learn react link'', () => {
   render(<App />);
